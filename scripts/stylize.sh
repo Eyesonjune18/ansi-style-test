baa9752a3becb33f11164c973b36c86d2dddbfa1
Lowell Thoerner <lowellashton@gmail.com>
2023-05-26 17:12:55 -0700

    Made style script support multiple styles.

--- scripts/stylize.sh
+++ scripts/stylize.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 
-case $1 in
-    blink)
-        start="\x1b[5m"
-        end="\x1b[25m"
-        ;;
-    dim)
-        start="\x1b[2m"
-        end="\x1b[22m"
-        ;;
-    bold)
-        start="\x1b[1m"
-        end="\x1b[22m"
-        ;;
-    hide)
-        start="\x1b[8m"
-        end="\x1b[28m"
-        ;;
-    invert)
-        start="\x1b[7m"
-        end="\x1b[27m"
-        ;;
-    ital)
-        start="\x1b[3m"
-        end="\x1b[23m"
-        ;;
-    strike)
-        start="\x1b[9m"
-        end="\x1b[29m"
-        ;;
-    uline)
-        start="\x1b[4m"
-        end="\x1b[24m"
-        ;;
-    *)
-        echo "Usage: $0 <blink|dim|bold|hide|invert|ital|strike|uline>"
-        exit 1
-        ;;
-esac
+start="\x1b["
 
+# Check if any arguments are provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <style1> [<style2> ...]"
+    exit 1
+fi
+
+# Iterate through each argument and construct the escape sequence
+while [ $# -gt 0 ]; do
+    case $1 in
+        blink)
+            start+="5;"
+            ;;
+        dim)
+            start+="2;"
+            ;;
+        bold)
+            start+="1;"
+            ;;
+        hide)
+            start+="8;"
+            ;;
+        invert)
+            start+="7;"
+            ;;
+        ital)
+            start+="3;"
+            ;;
+        strike)
+            start+="9;"
+            ;;
+        uline)
+            start+="4;"
+            ;;
+        *)
+            echo "Invalid style: $1"
+            echo "Usage: $0 <style1> [<style2> ...]"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Remove the trailing semicolon from the escape sequence
+start="${start%?}m"
+
+# Read input from stdin and apply the constructed escape sequence
 while read -r line; do
-    echo -e "$start$line$end"
+    echo -e "$start$line\x1b[0m"
 done <&0
